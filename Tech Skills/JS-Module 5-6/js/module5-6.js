9deba76300070d7edef57ce1c5b7748a2fc87d81
Your Name <you@example.com>
2016-03-21 13:59:28 +0200

    ended js 5-6

--- Tech Skills/JS-Module 5-6/js/module5-6.js
+++ Tech Skills/JS-Module 5-6/js/module5-6.js
@@ -1,47 +1,69 @@
 ﻿var timerObj = {
-    _timer : null,
-    _currentMsecValue: 0,
-    _isTimerWorking : false,
+    _timer: null,
+    _timerValue : 0,            // к-во мсек, которые таймер отработал от последнего старта
+    _startMsecValue : 0,        // время очередного запуска ожидания таймера
     _displayTime: function () {
-        document.getElementsByClassName('time-value')[0].innerHTML = 'sdasdads';
-        document.getElementsByClassName('msec')[0].innerHTML = (this._currentMsecValue % 1000) + '';
+        var msecs = this._timerValue % 1000;
+        var secs = Math.floor(this._timerValue / 1000) % 60;
+        var mins = Math.floor(this._timerValue / (60 * 1000)) % 60;
+        var hours = Math.floor(this._timerValue / (60 * 60 * 1000));
+        document.getElementsByClassName('time-value')[0].innerHTML =
+            this._numToString(hours) + ':'+
+            this._numToString(mins) + ':'+
+            this._numToString(secs);
+        document.getElementsByClassName('msec')[0].innerHTML = msecs + '';
     },
-    setTimeValue : function(msec){
-        this._currentMsecValue=msec;
+    _numToString(numValue){
+        var str=numValue.toString();
+        if (str.length > 2)
+            return '!!';
+        if (str.length ==1 )
+            return '0'+str;
+        return str;
+    },
+    _incrementTime: function () {
+        var dNow = Date.now();
+        this._timerValue += dNow - this._startMsecValue;
+        this._startMsecValue = dNow;
         this._displayTime();
     },
-    getFlagWorking: function () {
-        return this._isTimerWorking;
+    isWorking : function () {
+        return this._timer !=null;
     },
-    setFlagWorking: function (isWork) {
-        this._isTimerWorking = isWork;
+    run: function () {
+        this._startMsecValue = Date.now();
+        this._timer = setInterval(this._incrementTime.bind(this), 37); // Марианна, вот нравится мне число 37 миллисекунд и все тут,
+    },                                                                  // это нельзя понять, это надо запомнить :-)
+    stop : function() {
+        if( this._timer != null) {
+            clearInterval(this._timer);
+            this._timer = null;
+        }
     },
-    reverseFlagWorking: function () {
-        this._isTimerWorking = !this._isTimerWorking;
-    },
-    incrementTime : function() {
-        this._currentMsecValue++;
+    clear : function () {
+        this.stop();
+        this._startMsecValue = 0;
+        this._timerValue = 0;
         this._displayTime();
-    },
-    startTimer: function () {
-        this._timer = setInterval(this.incrementTime, 1);
-        document.getElementsByClassName('btn')[0].innerHTML = 'Pause';
-    },
-    suspendTimer: function () {
-
-    },
-    resumeTimer: function () {
-
-    },
-    clearTimer : function () {
-        this.setTimeValue(0);
-        this.setFlagWorking(false);
-    },
-    initTimer : function () {
-        this.setTimeValue(0);
-        this.setFlagWorking(false);
-        document.getElementsByClassName('btn')[0].addEventListener('click', this.startTimer);
-        document.getElementsByClassName('btn')[1].addEventListener('click', this.clearTimer);
     }
 }
-timerObj.initTimer();
+
+function controlTimer(event) {
+    if( timerObj.isWorking() )
+    {       // нажата Pause
+        timerObj.stop();
+        event.target.innerHTML = 'Resume' ;
+    }
+    else
+    {       // нажата Start или Resume
+        timerObj.run();
+        document.getElementsByClassName('btn')[0].innerHTML = 'Pause' ;
+    }
+}
+function clearTimer() {
+    timerObj.clear();
+    document.getElementsByClassName('btn')[0].innerHTML = 'Start' ;
+}
+
+document.getElementsByClassName('btn')[0].addEventListener('click', controlTimer);
+document.getElementsByClassName('btn')[1].addEventListener('click', clearTimer);
