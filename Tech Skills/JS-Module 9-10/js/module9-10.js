1f148f466931e3df79601d62a31d12bc78cc8107
Anatoly Golkov <agolkov@inbox.ru>
2016-03-27 21:18:26 +0300

    started js 9-10

---
+++ Tech Skills/JS-Module 9-10/js/module9-10.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    var instance = $('.jcarousel').data('jcarousel');
+    $('.jcarousel-prev').on('click', function (event) {
+        instance.scroll('-=1');
+    });
+    $('.jcarousel-next').on('click', function (event) {
+        instance.scroll('+=1');
+    });
+});
+/*    var instance = ;
+
+  instance*/
+ /*   $('.jcarousel ul').jcarousel({
+        list: '.jcarousel ul'
+    });
+    $('.jcarousel-prev').jcarouselControl({
+        target: '-=1'
+    });
+
+    $('.jcarousel-next').jcarouselControl({
+        target: '+=1'
+    });*/
