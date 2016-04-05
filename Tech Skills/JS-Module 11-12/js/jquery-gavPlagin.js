daf82a06305d5adf6d6e7e7c3d6aa93e8a82fe8b
Your Name <you@example.com>
2016-04-05 15:52:09 +0300

    started js 11-12

---
+++ Tech Skills/JS-Module 11-12/js/jquery-gavPlagin.js
@@ -0,0 +1,17 @@
+﻿(function ($) {
+    // значения по умолчанию 
+    var defaults = {
+                    listLength : 5,         // к-во видимых слайдов
+                    itemWidth  : '500px',   // высота
+                    itemHeight : '500px'    // и ширина каждого слайда
+                   };
+    // актуальные настройки
+    var options;
+
+    $.fn.gavPlugin = function (params) {
+        // options в extend нужна для сохранения параметров при многократном вызове
+        options = $.extend({}, defaults, options, params);
+
+        return this;
+    };
+})(jQuery);
