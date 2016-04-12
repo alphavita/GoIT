b4974b6286db4d026c5eeddabf4f148410cdb6e8
Your Name <you@example.com>
2016-04-12 16:56:29 +0300

    working. js 15-16 part1

--- Tech Skills/JS-Module 15-16/Part1/js/module15-16-part1.js
+++ Tech Skills/JS-Module 15-16/Part1/js/module15-16-part1.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 function GoogleCallback(queryObj,data) {
-
+    alert('callback!!!');
 }
 $(function () {
     $('form').on('submit', function (e) {
@@ -12,9 +12,21 @@ $(function () {
         }
         $.ajax({
             url: "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=" + searchText + "&rsz=large&callback=GoogleCallback&context=?",
+//            url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + /*searchText*/'test' + '&rsz=large&callback=GoogleCallback&context=?',
             datatype: 'jsonp',
-            error: function (err) {
-                alert('Ошибка'+err);
+            crossDomain: true, 
+            beforeSend: function (xhr) {
+//                xhr.setRequestHeader('Access-Control-Allow-Origin','*');
+//                xhr.setRequestHeader('X-Content-Type-Options','nosniff');
+//                xhr.setRequestHeader('Access-Control-Allow-Headers', 'X-ACCESS_TOKEN, Access-Control-Allow-Origin, Authorization, Origin, x-requested-with, Content-Type, Content-Range, Content-Disposition, Content-Description');
+//                xhr.setRequestHeader('Access-Control-Allow-Headers','origin, x-requested-with, content-type');
+//                xhr.setRequestHeader('Access-Control-Allow-Methods','PUT, GET, POST, DELETE, OPTIONS');
+            },
+            success: function(data){
+                alert('все ок'+ data);
+            }, 
+            error: function (xhr, textStatus, errorThrown) {
+                alert('Ошибка с кодом '+xhr.status);
             }
         });
     });
