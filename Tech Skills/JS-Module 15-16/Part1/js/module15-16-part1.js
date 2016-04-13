9fe350e7c60bbd0c17ab992d76eb88df05db279f
Your Name <you@example.com>
2016-04-13 16:52:51 +0300

    working.. js 15-16 part1

--- Tech Skills/JS-Module 15-16/Part1/js/module15-16-part1.js
+++ Tech Skills/JS-Module 15-16/Part1/js/module15-16-part1.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 function GoogleCallback(queryObj,data) {
-    alert('callback!!!');
+    alert(data.response);
 }
 $(function () {
     $('form').on('submit', function (e) {
@@ -11,22 +11,26 @@ $(function () {
             return this;
         }
         $.ajax({
-            url: "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=" + searchText + "&rsz=large&callback=GoogleCallback&context=?",
-//            url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' + /*searchText*/'test' + '&rsz=large&callback=GoogleCallback&context=?',
+            url: "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=" + searchText + "&callback=GoogleCallback&context=?",
             datatype: 'jsonp',
-            crossDomain: true, 
+            method : 'GET',
             beforeSend: function (xhr) {
+//                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+//                xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 //                xhr.setRequestHeader('Access-Control-Allow-Origin','*');
+//                xhr.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
+//                xhr.setRequestHeader('Access-Control-Allow-Methods','PUT, GET, POST, DELETE, OPTIONS');
+
+
+
 //                xhr.setRequestHeader('X-Content-Type-Options','nosniff');
 //                xhr.setRequestHeader('Access-Control-Allow-Headers', 'X-ACCESS_TOKEN, Access-Control-Allow-Origin, Authorization, Origin, x-requested-with, Content-Type, Content-Range, Content-Disposition, Content-Description');
-//                xhr.setRequestHeader('Access-Control-Allow-Headers','origin, x-requested-with, content-type');
-//                xhr.setRequestHeader('Access-Control-Allow-Methods','PUT, GET, POST, DELETE, OPTIONS');
             },
             success: function(data){
                 alert('все ок'+ data);
             }, 
             error: function (xhr, textStatus, errorThrown) {
-                alert('Ошибка с кодом '+xhr.status);
+                alert('Ошибка с кодом '+errorThrown);
             }
         });
     });
