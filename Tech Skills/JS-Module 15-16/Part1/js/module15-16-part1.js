5877d83213df82752d8c61d29e17f25c03ae36cc
Anatoly Golkov <agolkov@inbox.ru>
2016-04-14 00:14:18 +0300

    working.... js 15-16 Part1

--- Tech Skills/JS-Module 15-16/Part1/js/module15-16-part1.js
+++ Tech Skills/JS-Module 15-16/Part1/js/module15-16-part1.js
@@ -1,7 +1,6 @@
 ﻿'use strict';
-function GoogleCallback(queryObj,data) {
-    alert(data.response);
-}
+/*function GoogleCallback(queryObj, data) {}*/
+
 $(function () {
     $('form').on('submit', function (e) {
         e.preventDefault();
@@ -11,23 +10,13 @@ $(function () {
             return this;
         }
         $.ajax({
-            url: "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=" + searchText + "&callback=GoogleCallback&context=?",
-            datatype: 'jsonp',
+            url: "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=" + searchText,
+            dataType: 'jsonp',
             method : 'GET',
-            beforeSend: function (xhr) {
-//                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-//                xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-//                xhr.setRequestHeader('Access-Control-Allow-Origin','*');
-//                xhr.setRequestHeader('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
-//                xhr.setRequestHeader('Access-Control-Allow-Methods','PUT, GET, POST, DELETE, OPTIONS');
-
-
-
-//                xhr.setRequestHeader('X-Content-Type-Options','nosniff');
-//                xhr.setRequestHeader('Access-Control-Allow-Headers', 'X-ACCESS_TOKEN, Access-Control-Allow-Origin, Authorization, Origin, x-requested-with, Content-Type, Content-Range, Content-Disposition, Content-Description');
-            },
             success: function(data){
-                alert('все ок'+ data);
+                for (var i = 0; i < data.responseData.results.length; i++) {
+                    var $article = data.responseData.results[i];
+                }
             }, 
             error: function (xhr, textStatus, errorThrown) {
                 alert('Ошибка с кодом '+errorThrown);
