5f0927fc9f9c44385c17057128d8932ba2c63856
Your Name <you@example.com>
2016-04-14 16:44:47 +0300

    working... js 15-16 part1

--- Tech Skills/JS-Module 15-16/Part1/js/module15-16-part1.js
+++ Tech Skills/JS-Module 15-16/Part1/js/module15-16-part1.js
@@ -4,19 +4,29 @@
 $(function () {
     $('form').on('submit', function (e) {
         e.preventDefault();
+        var startNumberArticle = 1;
         var searchText = $('input[name="search-text"]', this).val();
         if (searchText.length == 0) {
             alert('Введите что-нибудь для поиска');
             return this;
         }
         $.ajax({
-            url: "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=" + searchText,
+            url: "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=" + searchText+
+                        '&start='+startNumberArticle+'&rsz=large',
             dataType: 'jsonp',
             method : 'GET',
-            success: function(data){
-                for (var i = 0; i < data.responseData.results.length; i++) {
-                    var $article = data.responseData.results[i];
+            success: function (data) {
+                $('.result-container div').remove();
+                if (data.responseStatus != 200) {
+                    alert('Ошибка поиска с кодом: ',data.responseStatus);
+                    return false;
                 }
+                if (data.responseData.results.length == 0) {
+                    alert('Ничего не найдено');
+                    return false;
+                }
+                var htmlData = tmpl('googleTemplate', data);       
+                $(htmlData).insertBefore('.result-container #googleTemplate');
             }, 
             error: function (xhr, textStatus, errorThrown) {
                 alert('Ошибка с кодом '+errorThrown);
