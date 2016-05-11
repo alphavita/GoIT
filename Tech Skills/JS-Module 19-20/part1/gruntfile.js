ad2dc57fc12eab6f96ba2827f394a8006f0a53b9
Your Name <you@example.com>
2016-05-11 16:36:51 +0300

    working dz19-20

---
+++ Tech Skills/JS-Module 19-20/part1/gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+	  concat: {
+/*            js: {
+                src: ['js/src/*.js'],
+                dest: 'js/dz17-18.min.js',
+                options: {
+                    separator: ';'
+                }
+            },*/
+            css: {
+                src: ['src/css/reset.css','src/css/fontello_dz_19-20.css','src/css/fontubuntu.css','src/css/default.css'],
+                dest: 'css/dz19-20.css',
+                options: {
+                    separator: '\n\r'
+                }
+            },
+	  },
+/*	  uglify: {
+	    dist: {
+	      src: ['js/dz17-18.min.js'],
+	      dest: 'js/dz17-18.min.js',
+	    },
+	  } ,*/
+	  cssmin: {
+  	    target: {
+    	      files: [{
+      	      	expand: true,
+      		cwd: 'css',
+      		src: ['dz19-20.css'],
+      		dest: 'css',
+      		ext: '.min.css'
+    	     }]
+  	   }
+	 },
+         sass: {
+		options: {
+			sourceMap: true
+		},
+		dist: {
+			files: {
+				'src/css/default.css': 'src/scss/default.scss'
+			}
+		}
+	},
+         watch: {
+                css: {
+                     files: ['src/scss/*.scss'],
+                     tasks: [/*'uglify',*/'sass','concat','cssmin'],
+                },
+         }   
+	});
+	grunt.loadNpmTasks('grunt-contrib-concat');
+/*	grunt.loadNpmTasks('grunt-contrib-uglify');                */
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');                   
+	grunt.loadNpmTasks('grunt-sass');	
+	
+grunt.registerTask('default',[/*'uglify',*/'sass','concat','cssmin']);
+}
