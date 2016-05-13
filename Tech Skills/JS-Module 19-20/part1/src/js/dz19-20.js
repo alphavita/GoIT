fea579e997333ff9058b1fb1a121d7d37316782d
Anatoly Golkov <agolkov@inbox.ru>
2016-05-13 23:18:58 +0300

    working js 19-20

--- Tech Skills/JS-Module 19-20/part1/src/js/dz19-20.js
+++ Tech Skills/JS-Module 19-20/part1/src/js/dz19-20.js
@@ -1,4 +1,6 @@
 ﻿$(function () {
+
+    // запускаем карусель
     var carousel = new  gavCarousel(0, 1, 1, {
                             carouselSelector: '.header__slider',
                             listSelector: '.slider__list',
@@ -11,4 +13,25 @@
     setInterval(function () {
         carousel.ScrollOffset(1);
     }, 5000);
+
+
+    // инициализируем аккордеон
+    $('.panel__description').hide();
+    $('.panel__active').find('.panel__description').show();
+    $('.panel__toggle').on('click', function () {
+        if (!$(this).parent().parent().hasClass('panel__active')) {
+            $('.accordion__panel').removeClass('panel__active');
+            $('.panel__toggle').html('+');
+            $(this).parent().parent().addClass('panel__active')
+            $(this).html('-');
+            var descBlocks = $('.panel__description');
+            descBlocks.slideUp();
+            $(this).parent().next().slideDown();
+        } else {
+            $(this).parent().parent().removeClass('panel__active');
+            $(this).html('+');
+            $(this).parent().next().slideUp();
+        }
+    });
+
 });
