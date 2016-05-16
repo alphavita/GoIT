5448b76a8c0a4011738efd25d8124f093713352a
Your Name <you@example.com>
2016-05-16 16:06:29 +0300

    started dz23-24

---
+++ Tech Skills/JS-Module 23-24/js/module23-24.js
@@ -0,0 +1,33 @@
+﻿requirejs.config({
+    paths: {
+        'jQuery': 'added-files/jquery-1.12.2.min',
+        'lodash': 'added-files/lodash.min',
+        'template-Resig' : 'added-files/template',
+    },
+    shim: {
+        'jQuery': {
+            exports: 'jQuery'
+        },
+        'lodash': {
+            exports: 'lodash'
+        },
+        'template-Resig': {
+            exports: 'tmpl'
+        }
+    }
+});
+require(
+    [
+        'module23-24_model',
+        'module23-24_view',
+        'module23-24_controller'
+    ],
+
+    function (model,view,controller) {
+        model.addItem('adsfsdfsdf');
+        model.addItem('adsfsdfsdf');
+        model.addItem('adsfsdfsdf');
+        model.removeItem('sfgdfgdfgfg');
+        view.showData(model.getItemsList());
+    }
+);
---
+++ Tech Skills/JS-Module 23-24/js/module23-24_controller.js
@@ -0,0 +1,6 @@
+﻿define(
+    'module23-24_controller',
+    ['jQuery','module23-24_model','module23-24_view'],
+    function ($,model,view) {
+    }
+);
